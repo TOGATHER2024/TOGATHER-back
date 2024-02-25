4344d946e1c5c43417ea83352a31b93a770371c5
D <deilee0913@gmail.com>
2024-02-25 17:20:43 +0900

    🍡New Project

---
+++ src/main/java/jusomejusome/togather/TogatherApplication.java
@@ -0,0 +1,13 @@
+package jusomejusome.togather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jusomejusome/togather/TogatherApplicationTests.java
@@ -0,0 +1,13 @@
+package jusomejusome.togather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
